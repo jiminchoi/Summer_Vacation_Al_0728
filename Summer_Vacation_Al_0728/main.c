c98e72aa9dd21c750bd1d095251509cf24b2e072
ChoiJimin <Producer@choijimin-no-macbook-pro.local>
2016-07-28 17:21:45 +0900

    Initial Commit

---
+++ Summer_Vacation_Al_0728/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Summer_Vacation_Al_0728
+//
+//  Created by ChoiJimin on 2016/07/28.
+//  Copyright © 2016年 ChoiJimin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
